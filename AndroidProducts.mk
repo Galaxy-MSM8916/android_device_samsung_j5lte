f467ac54e071d35ef9ceaa237bbf79fa6d52fe10
Zvikomborero VIncent Zvikaramba <zvikovincent@gmail.com>
2016-07-18 22:51:47 -0400

    Removed more CM references.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,3 @@
+LOCAL_PATH := device/samsung/gprimeltecan
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_gprimeltecan.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,4 @@
 for i in eng userdebug user; do
-	add_lunch_combo cm_gprimeltecan-${i}
-#	add_lunch_combo omni_gprimeltecan-${i}
-#	add_lunch_combo full_gprimeltecan-${i}
+	add_lunch_combo omni_gprimeltecan-${i}
+	add_lunch_combo full_gprimeltecan-${i}
 done
