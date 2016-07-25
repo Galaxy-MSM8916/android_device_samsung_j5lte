bdc76af3a49347b1aaa14ad8998a274c4d613b12
Zvikomborero VIncent Zvikaramba <zvikovincent@gmail.com>
2016-07-25 03:15:42 -0400

    Ucommented if statement.

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,4 +1,4 @@
-#ifeq ($(TARGET_PROVIDES_CAMERA_HAL),true)
+ifeq ($(TARGET_PROVIDES_CAMERA_HAL),true)
 
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
@@ -18,5 +18,5 @@ LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
 
-#endif
+endif
 
