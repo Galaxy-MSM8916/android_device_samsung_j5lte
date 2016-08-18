8799532ad7dbce126a46b3f2451a147804dd5930
Zvikomborero VIncent Zvikaramba <zvikovincent@gmail.com>
2016-08-18 17:12:40 -0400

    Added qcom radio script as prebuilt file.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -205,6 +205,16 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 ######################
+### init.qcom.fm.sh
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.fm.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := ../recovery/$(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)/etc
+include $(BUILD_PREBUILT)
+
+######################
 ### twrp.fstab
 include $(CLEAR_VARS)
 LOCAL_MODULE       := twrp.fstab
