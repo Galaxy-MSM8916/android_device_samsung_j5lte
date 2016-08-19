cadd54f7db6a15b75ad1e21b15498c09c3386708
Zvikomborero VIncent Zvikaramba <zvikovincent@gmail.com>
2016-08-19 19:59:34 -0400

    Updated InputDisabler touch screen path

--- InputDisabler/src/org/davis/inputdisabler/Constants.java
+++ InputDisabler/src/org/davis/inputdisabler/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
 
     }
 
-    public static final String TS_PATH = "/sys/class/input/input3/enabled";
+    public static final String TS_PATH = "/sys/class/input/input2/enabled";
 
 	// Doze intent
 
