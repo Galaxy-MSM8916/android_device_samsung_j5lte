9f4ae07604a461c83372d396760bd1e5a7b25254
Zvikomborero VIncent Zvikaramba <zvikovincent@gmail.com>
2016-08-18 22:41:11 -0400

    Fixed typo in rootdir Makefile

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -210,7 +210,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.fm.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := ../recovery/$(LOCAL_MODULE)
+LOCAL_SRC_FILES    := etc/$(LOCAL_MODULE)
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)/etc
 include $(BUILD_PREBUILT)
 
