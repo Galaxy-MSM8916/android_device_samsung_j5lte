0650dd4bf62577fbc35ffe4427ae8867d93a6667
Zvikomborero VIncent Zvikaramba <zvikovincent@gmail.com>
2016-08-28 23:57:06 -0400

    Enabled building of dt image from source

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -1,13 +1,21 @@
 LOCAL_PATH := $(call my-dir)
 
 ## Build and run dtbtool
-INSTALLED_DTIMAGE_TARGET := $(LOCAL_PATH)/dt.img
+DTBTOOL := $(HOST_OUT_EXECUTABLES)/dtbToolCM$(HOST_EXECUTABLE_SUFFIX)
+INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
 
 ifneq ($(TARGET_KERNEL_ARCH),)
 KERNEL_ARCH := $(TARGET_KERNEL_ARCH)
 else
 KERNEL_ARCH := $(TARGET_ARCH)
 endif
+
+#generate the dtimage
+$(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr $(INSTALLED_KERNEL_TARGET)
+	$(call pretty,"Target dt image: $(INSTALLED_DTIMAGE_TARGET)")
+	$(hide) $(DTBTOOL) -2 -o $(INSTALLED_DTIMAGE_TARGET) -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/
+	@echo -e ${CL_CYN}"Made DT image: $@"${CL_RST}
+
 ## Overload bootimg generation: Same as the original, + --dt arg
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(INSTALLED_DTIMAGE_TARGET)
 	$(call pretty,"Target boot image: $@")
