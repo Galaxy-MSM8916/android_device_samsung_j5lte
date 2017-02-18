8771505e18f168b60485003dee4ce2b78297d104
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-02-18 02:02:27 -0500

    gprimelte: Remove redundant Makefile

--- lineage.mk
+++ lineage.mk
@@ -2,7 +2,7 @@
 # Inherit from common
 $(call inherit-product, device/samsung/gprimelte-common/lineage.mk)
 
-$(call inherit-product, device/samsung/gprimelte/full_gprimelte.mk)
+$(call inherit-product, device/samsung/gprimelte/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gprimelte
