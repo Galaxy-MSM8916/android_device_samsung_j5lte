eb91af58d69a72bc3a372ac900de6c23e1275fc5
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-03-12 03:35:50 -0400

    gprimelte: Move acdbs to vendor tree

--- device.mk
+++ device.mk
@@ -11,13 +11,6 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/gprimelte/overlay
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/audio/Bluetooth_cal.acdb:system/etc/Bluetooth_cal.acdb \
-	$(LOCAL_PATH)/audio/General_cal.acdb:system/etc/General_cal.acdb \
-	$(LOCAL_PATH)/audio/Global_cal.acdb:system/etc/Global_cal.acdb \
-	$(LOCAL_PATH)/audio/Handset_cal.acdb:system/etc/Handset_cal.acdb \
-	$(LOCAL_PATH)/audio/Hdmi_cal.acdb:system/etc/Hdmi_cal.acdb \
-	$(LOCAL_PATH)/audio/Headset_cal.acdb:system/etc/Headset_cal.acdb \
-	$(LOCAL_PATH)/audio/Speaker_cal.acdb:system/etc/Speaker_cal.acdb \
 	$(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
 # Media configurations
