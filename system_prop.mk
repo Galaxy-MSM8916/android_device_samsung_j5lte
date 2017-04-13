3b2762a6515235938322d999980bcd78356f1e39
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-04-12 23:16:10 -0400

    system_prop: Add telephony.lteOnCdmaDevice property

--- system_prop.mk
+++ system_prop.mk
@@ -9,6 +9,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # RIL
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	telephony.lteOnCdmaDevice=0 \
 	ro.multisim.simslotcount=2 \
 	persist.radio.multisim.config=dsds \
 	rild.libpath2=/system/lib/libsec-ril-dsds.so
