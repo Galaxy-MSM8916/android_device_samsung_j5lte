f8d96e0a6060cdb48812eb4ab3828afbebf6102f
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-05-28 02:49:29 -0400

    j5: Commonise audio configuration files

--- device.mk
+++ device.mk
@@ -11,14 +11,3 @@ LOCAL_PATH := device/samsung/j5ltexx
 
 # Common overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-
-# Audio configuration
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
-
-# Media configurations
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/media/media_codecs_8929.xml:system/etc/media_codecs_8929.xml \
-	$(LOCAL_PATH)/media/media_codecs_8939.xml:system/etc/media_codecs_8939.xml \
-	$(LOCAL_PATH)/media/media_codecs.xml:system/etc/media_codecs.xml \
-	$(LOCAL_PATH)/media/media_profiles.xml:system/etc/media_profiles.xml
