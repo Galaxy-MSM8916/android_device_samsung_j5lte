77ca3ac04b8baa5ca57e732837b6a7d5dd722c0f
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2018-09-24 20:53:57 -0400

    j5lte: Update AndroidProducts makefile
    Consider {lineage,rr,omni,pa}_j5lte the AOSP way
    
    Change-Id: I34e85f02cf8d6190da020e3afa56f03b24cba5ac

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,6 @@
 LOCAL_PATH := device/samsung/j5lte
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_j5lte.mk
+	$(LOCAL_DIR)/omni_j5lte.mk \
+	$(LOCAL_DIR)/rr_j5lte.mk \
+	$(LOCAL_DIR)/pa_j5lte.mk \
+	$(LOCAL_DIR)/lineage_j5lte.mk
