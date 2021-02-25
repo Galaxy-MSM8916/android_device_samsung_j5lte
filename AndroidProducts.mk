4aa6c5fb66351a57cb48f5783ca4e20e76e526e3
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-25 17:42:47 +0100

    j5lte: Remove non-Lineage products
    Change-Id: Ifec4b64382f45b5944ab0ed4958c1f7c78692f77

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,5 @@
 LOCAL_PATH := device/samsung/j5lte
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/omni_j5lte.mk \
-	$(LOCAL_DIR)/rr_j5lte.mk \
-	$(LOCAL_DIR)/pa_j5lte.mk \
 	$(LOCAL_DIR)/lineage_j5lte.mk
 
 COMMON_LUNCH_CHOICES := \
